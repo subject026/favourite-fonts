28f394f725c22584c0329772ba0191073168f098
subject026 <sudobuildspeakers@gmail.com>
2019-10-13 18:06:43 +0100

    removed unnecessary imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import styled from "styled-components";
-import { ThemeProvider, css } from "styled-components";
+import { ThemeProvider } from "styled-components";
 
 import Header from "./Header";
 import OptionsBar from "./OptionsBar";
