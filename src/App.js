5dcb81147c0a2ef6babde8391e024fdee2ac1f01
subject026 <sudobuildspeakers@gmail.com>
2019-10-15 14:24:48 +0100

    added global styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 
 import Header from "./components/Header";
 import OptionsBar from "./components/OptionsBar";
@@ -7,10 +7,18 @@ import Footer from "./components/Footer";
 
 import { Theme, CardGrid } from "./styled-components";
 
+const GlobalStyle = createGlobalStyle`
+  body, html {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
 class App extends React.Component {
   render() {
     return (
       <ThemeProvider theme={Theme}>
+        <GlobalStyle />
         <Header />
         <OptionsBar />
         <CardGrid>{/* Some cards here */}</CardGrid>
