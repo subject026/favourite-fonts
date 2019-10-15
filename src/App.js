9da2dbecf5e6b0eda080d4beec7e43f11958f586
subject026 <sudobuildspeakers@gmail.com>
2019-10-15 14:48:35 +0100

    began API experiments

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Header from "./components/Header";
 import OptionsBar from "./components/OptionsBar";
 import Footer from "./components/Footer";
 
-import { Theme, CardGrid } from "./styled-components";
+import { Theme, CardGrid, FontCard } from "./styled-components";
+
+import config from "./config";
 
 const GlobalStyle = createGlobalStyle`
   body, html {
@@ -15,13 +17,44 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 class App extends React.Component {
+  state = {
+    fonts: []
+  };
+
+  componentDidMount = async () => {
+    try {
+      const res = await fetch(
+        `https://www.googleapis.com/webfonts/v1/webfonts?key=${config.API_KEY}&sort=popularity`
+      );
+      const data = await res.json();
+      this.setState(state => {
+        return {
+          ...state,
+          fonts: [...data.items.slice(0, 5)]
+        };
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   render() {
+    const { fonts } = this.state;
     return (
       <ThemeProvider theme={Theme}>
         <GlobalStyle />
         <Header />
         <OptionsBar />
-        <CardGrid>{/* Some cards here */}</CardGrid>
+        <CardGrid>
+          {fonts.map(font => {
+            console.dir(font);
+            return (
+              <FontCard fontFamily={font.family}>
+                <div>{font.family}</div>
+              </FontCard>
+            );
+          })}
+        </CardGrid>
         <Footer />
       </ThemeProvider>
     );
