b94217d386cadbb7e3d86dba7d7be16a8e377025
subject026 <sudobuildspeakers@gmail.com>
2019-10-19 08:41:42 +0100

    implemented card grid

--- src/styled-components/index.js
+++ src/styled-components/index.js
@@ -5,8 +5,13 @@ export const Theme = {
   color1: "blue"
 };
 
-export const CardGrid = styled.section``;
+export const CardGrid = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 15px;
+`;
 
 export const FontCard = styled.div`
   font-size: 30px;
+  border: 3px solid darkmagenta;
 `;
