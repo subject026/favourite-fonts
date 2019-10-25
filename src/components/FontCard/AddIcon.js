4859b0e577385803efb20b1417fd1d5e89e6cd3e
subject026 <sudobuildspeakers@gmail.com>
2019-10-25 13:03:33 +0100

    brought in icons

---
+++ src/components/FontCard/AddIcon.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ResetIcon = () => {
+  return (
+    <div>
+      <svg width="24" height="24" fill="none">
+        <path fill="#fff" d="M0 0h24v24H0z" />
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M2 12C2 6.48 6.48 2 12 2s10 4.48 10 10-4.48 10-10 10S2 17.52 2 12zm11 1h4v-2h-4V7h-2v4H7v2h4v4h2v-4z"
+          fill="#000"
+          fill-opacity=".54"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default ResetIcon;
---
+++ src/components/OptionsBar/SearchIcon.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const SearchIcon = () => {
+  return (
+    <div>
+      <svg width="24" height="24" fill="none">
+        <path fill="#fff" d="M0 0h24v24H0z" />
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M14.71 14h.79l4.99 5L19 20.49l-5-4.99v-.79l-.27-.28C12.59 15.41 11.11 16 9.5 16 5.91 16 3 13.09 3 9.5S5.91 3 9.5 3 16 5.91 16 9.5c0 1.61-.59 3.09-1.57 4.23l.28.27zM5 9.5C5 11.99 7.01 14 9.5 14S14 11.99 14 9.5 11.99 5 9.5 5 5 7.01 5 9.5z"
+          fill="#000"
+          fill-opacity=".54"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default SearchIcon;
