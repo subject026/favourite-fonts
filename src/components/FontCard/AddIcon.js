6e258f541f458aeb6e8d749fd072555691997f97
subject026 <sudobuildspeakers@gmail.com>
2019-10-25 17:36:53 +0100

    fixed svg attribute errors

--- src/components/FontCard/AddIcon.js
+++ src/components/FontCard/AddIcon.js
@@ -6,11 +6,11 @@ const ResetIcon = () => {
       <svg width="24" height="24" fill="none">
         <path fill="#fff" d="M0 0h24v24H0z" />
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M2 12C2 6.48 6.48 2 12 2s10 4.48 10 10-4.48 10-10 10S2 17.52 2 12zm11 1h4v-2h-4V7h-2v4H7v2h4v4h2v-4z"
           fill="#000"
-          fill-opacity=".54"
+          fillOpacity=".54"
         />
       </svg>
     </div>
