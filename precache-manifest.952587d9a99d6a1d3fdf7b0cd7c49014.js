7f79649575ced2f5b50b6ea6cd6b13e4a09e519a
subject026 <sudobuildspeakers@gmail.com>
2019-10-25 17:41:31 +0100

    Updates

---
+++ precache-manifest.952587d9a99d6a1d3fdf7b0cd7c49014.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea23ae9e5c5fbfd44dc60150b2cf6d56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c911239d90f51cf955ec",
+    "url": "/static/js/2.9762314e.chunk.js"
+  },
+  {
+    "revision": "c9106a8969a23ca72cf7",
+    "url": "/static/js/main.ed033582.chunk.js"
+  },
+  {
+    "revision": "9181b8ba347d9e07c55c",
+    "url": "/static/js/runtime-main.ee86dabb.js"
+  }
+]);
