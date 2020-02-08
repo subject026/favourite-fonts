5d09a0125d4f299867af9009376e71f89725ab6d
subject026 <sudobuildspeakers@gmail.com>
2020-02-08 12:28:30 +0000

    refactored media queries

--- src/mixins.js
+++ src/mixins.js
@@ -1,9 +1,25 @@
 import { css } from "styled-components";
 
-export const someMixin = () => {
-  return css`
-    display: none;
-  `;
-};
+// variables
 
 export const $nav_transition = "0.3s cubic-bezier(0.215, 0.61, 0.355, 1)";
+
+// media queries
+
+const buildMinWidthQueries = widths => {
+  const queries = widths.reduce((acc, width) => {
+    acc[`from${width}`] = styles => {
+      return css`
+        @media (min-width: ${width}) {
+          ${styles}
+        }
+      `;
+    };
+    return acc;
+  }, {});
+  return queries;
+};
+
+export const width = buildMinWidthQueries(["600px", "730px"]);
+
+//
